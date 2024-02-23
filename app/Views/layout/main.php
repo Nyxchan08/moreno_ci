39821d986d35a25e2d1cd6765344f959874804b0
Nyxchan08 <christiandv.moreno@gmail.com>
2024-02-23 16:05:59 +0800

    first commit

---
+++ app/Views/layout/main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?=base_url()?>public/css/bootstrap.min.css">
+</head>
+<body>
+    <?=$this->renderSection('content')?>
+    <script src="<?=base_url()?>public/js/bootstrap.bundle.js"></script>
+    
+</body>
+</html>
---
+++ app/Views/user/add.php
@@ -0,0 +1,9 @@
+<?=$this->extend('layout/main')?>
+
+<?=$this->section('content')?>
+
+<h1 style="text-align: center;">This is user add page.</h1>
+
+<p style="text-align: center;"><em>This a paragraph</em></p>
+
+<?=$this->endSection('content')?>
